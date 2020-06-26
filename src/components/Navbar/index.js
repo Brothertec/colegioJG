481920b00205c9c75a2e01f1e3e3dddd93a46699
Everson Santos <everson.santos.f@gmail.com>
2020-06-26 13:57:47 -0300

    Navbar inacabada mobile

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
-// import logoImg from '';
+import logoJG from '../../image/logo-colegio-jardim-getsemani.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faGraduationCap, faMusic,faPencilRuler, faHandHoldingHeart, faLock} from '@fortawesome/free-solid-svg-icons';
+
+
 
 
 import './nav.css';
@@ -8,41 +12,39 @@ import './nav.css';
 
 export default function Navbar() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="#">Navbar</a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul className="navbar-nav">
-                    <li className="nav-item active">
-                        <a className="nav-link" href="#">Início <span className="sr-only">(current)</span></a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Sobre Nós</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Musicalização</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Aulas</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Doações</a>
-                    </li>
-                    <li className="nav-item dropdown">
-                        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Dropdown link
-              </a>
-                        <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                            <a className="dropdown-item" href="#">Action</a>
-                            <a className="dropdown-item" href="#">Another action</a>
-                            <a className="dropdown-item" href="#">Something else here</a>
-                        </div>
-                    </li>
-                </ul>
-            </div>
-        </nav>
+        <div className="fundo">
+            <nav className="navbar navbar-expand-lg fundo-navbar">
+                <div className="container">
+                    <a className="navbarlogo" href="#">
+                        <img className="logo" src={logoJG} alt="logo" />
+                    </a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon botao"></span>
+                    </button>
+                    <div className="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <a className="nav-link navitem--marcador" href="#"><span className="icone"><FontAwesomeIcon  icon={faHome}/></span>Início <span className="sr-only">(current)</span></a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link navitem--marcador" href="#"><span className="icone"><FontAwesomeIcon icon={faGraduationCap}/></span>Sobre Nós</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link navitem--marcador" href="#"><span className="icone"><FontAwesomeIcon icon={faMusic}/></span>Musicalização<span className="icone2"><FontAwesomeIcon   icon={faLock}/></span></a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link navitem--marcador" href="#"><span className="icone"><FontAwesomeIcon icon={faPencilRuler}/></span>Aulas<span className="icone2"><FontAwesomeIcon  icon={faLock}/></span></a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link navitem--marcador" href="#"><span className="icone"><FontAwesomeIcon icon={faHandHoldingHeart}/></span>Doações</a>
+                            </li>
+
+
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
     )
 
 };
