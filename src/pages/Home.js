920c95295d17aa055e6ee4748d1c09295b5149aa
Everson Santos <everson.santos.f@gmail.com>
2020-07-10 19:55:08 -0300

    doações sem terminar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,16 +4,17 @@ import AprendizadoAliado from '../components/compositions/AprendizadoAliado';
 import AreaAluno from '../components/compositions/AreaAlunos';
 import Mapa from '../components/compositions/Mapa';
 import Navbar from '../components/Navbar';
+import Doacao from '../components/compositions/Doacoes'
 
 
 export default function Home() {
     return(
         <div className="home">
-            <Navbar/>
-            <InfoEscola/>
+            <Doacao/>
+            {/* <InfoEscola/>
             <AprendizadoAliado/>
             <AreaAluno/>
-            <Mapa/>
+            <Mapa/> */}
         </div>
     );
 }
