307844f4740dd0812acbb1cc9b4c912ff4372019
Romulo Gomes <romulo.redg@gmail.com>
2020-07-10 17:09:02 -0300

    Adição da navigar na Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,11 +3,13 @@ import InfoEscola from '../components/compositions/InfoEscola';
 import AprendizadoAliado from '../components/compositions/AprendizadoAliado';
 import AreaAluno from '../components/compositions/AreaAlunos';
 import Mapa from '../components/compositions/Mapa';
+import Navbar from '../components/Navbar';
 
 
 export default function Home() {
     return(
         <div className="home">
+            <Navbar/>
             <InfoEscola/>
             <AprendizadoAliado/>
             <AreaAluno/>
